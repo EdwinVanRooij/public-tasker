366bc0f8682245a841de09feb35ac5083e917832
Edwin <edwinvrooij@gmail.com>
2020-05-16 16:47:04 +0200

    fix

--- achievements/1_updateAllAchievements.js
+++ achievements/1_updateAllAchievements.js
@@ -8,11 +8,12 @@ if (typeof http_data === "undefined") {
     // We're developing locally, initialize sample data.
     isDevelopmentEnvironment = true;
     data = {
-          "range": "'Prep Lists & Event Lists'!G17:J242", "majorDimension": "ROWS", "values": [ [ "Douchen", "nvt", "nvt" ], [ "", "T: Zeep gebruiken" ], [ "", "T: Shampoo gebruiken" ], [ "", "N: Gezicht scrubben", "Max 2x per week, het liefst maandag & donderdag" ], [ "", "N: Gezichtscreme gebruiken", "Elke keer" ],
-            [], [ "Naar fitness gaan", "nvt", "nvt" ], [ "", "V: Oortjes meenemen" ], [ "", "V: Euro meenemen" ], [ "", "V: Fitness pas meenemen" ], [], [ "Op stap gaan", "nvt", "nvt", "Douchen" ], [ "", "V: Oordopjes meenemen" ], [ "", "V: Tanden poetsen" ], [ "", "V: Listerine gebruiken" ], [ "", "V: Ketting poetsen & omdoen" ], [ "", "V: Mobiel opladen" ], [ "", "V: P/E: Douchen" ], [ "", "V: Scheerapparaat opladen" ], [ "", "V: Oksels, beneden, gezicht (scheren/trimmen)" ],
-            [ "", "V: Haren doen" ], [ "", "V: Deo & parfum opdoen" ], [ "", "V: ID, geld/pinpas meenemen" ], [ "", "V: Stapschoenen aandoen" ], [ "", "V: Lenzendoosje + vloeistof meenemen" ], [], [], [ "Naar festival gaan", "nvt", "nvt", "Op stap gaan" ], [ "", "V: Powerbank opladen" ], [ "", "V: Nuts eten" ], [ "", "V: Bananen eten" ], [ "", "V: Powerbank meenemen" ], [ "", "V: Mobiel opladen" ], [ "", "V: Haren doen" ], [ "", "V: Zonnebrillen meenemen" ], [ "", "V/T: Eten" ], [ "",
-              "V/T: Drinken" ], [ "", "V: Tanden poetsen" ], [ "", "V: Snoep meenemen" ], [ "", "V: Waaier(s) meenemen" ], [ "", "V: Pet meenemen" ], [ "", "V: Waterzak(ken) meenemen" ], [ "", "V: Paraplu meenemen" ], [ "", "V: OV chipkaart meenemen"
-            ]]
+          "values": [
+              [ "description 1", "date", "2", "toelichting", "keyword" ],
+              [ "description 2", "date", "3", "toelichting", "keyword" ],
+              [ "description 3", "date", "4", "toelichting", "keyword" ],
+              [ "description 4", "date", "5", "toelichting", "keyword" ],
+            ]
     };
 } else {
     //  We're in tasker, use the real data.
@@ -39,7 +40,7 @@ var achievements = [];
 data.values.forEach(function(row) {
     var description = row[0];
     var days = row[2];
-    var keyWords = row[3];
+    var keyWords = row[4];
     var achievement = new Achievement(description, days, keyWords);
     achievements.push(achievement);
 });
